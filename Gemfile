a499e2e4bf9a5621a05903da6dfee9002914a812
Jakub Polak <jakub.polak@goodylabs.com>
2015-07-04 01:43:44 +0200

    Install Rubocop and apply the ruby style.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source 'https://rubygems.org'
-
+gem 'rubocop'
 # Specify your gem's dependencies in zip-codes.gemspec
 gemspec
--- lib/zip-codes.rb
+++ lib/zip-codes.rb
@@ -4,14 +4,14 @@ module ZipCodes
   VERSION = '0.2.1'
 
   class << self
-    def identify code
+    def identify(code)
       db[code]
     end
 
     def db
       @db ||= begin
         this_file = File.expand_path(File.dirname(__FILE__))
-        us_data = File.join(this_file, "data", "US.yml")
+        us_data = File.join(this_file, 'data', 'US.yml')
         YAML.load(File.open(us_data))
       end
     end
