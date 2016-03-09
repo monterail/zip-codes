721e6358bef1e0d2d27687a81d293b2ba06afdd2
student <student@student.com>
2016-03-09 15:56:36 +0100

    repaired for just one loading file by task on country

--- lib/zip-codes.rb
+++ lib/zip-codes.rb
@@ -1,24 +1,36 @@
 require 'yaml'
 
 module ZipCodes
-  VERSION = '0.2.2'
+  VERSION = '0.2.1'
 
   class << self
 
-    def identify(code, countries = ["US"])
-      db(countries)[code]
+    def init_db(arr)
+      @db = nil
+      @countries = arr
     end
 
-    def db(countries)
-      hash = {}
-      countries.each do |country|
-        this_file = File.expand_path(File.dirname(__FILE__))
-        us_data = File.join(this_file, 'data', "#{country}.yml")
-        tmp = YAML.load(File.open(us_data))
-        hash.merge!(tmp)
+    def load_db
+      @db ||= begin
+        hash = {}
+        @countries.each do |country|
+          this_file = File.expand_path(File.dirname(__FILE__))
+          us_data = File.join(this_file, 'data', "#{country}.yml")
+          tmp = YAML.load(File.open(us_data))
+          hash.merge!(tmp)
+        end
+        hash
       end
+      @db
+    end
 
-      hash
+    def identify(code, countries = ["US"])
+      init_db(countries) if @countries == nil || (countries.sort != @countries.sort)
+      db[code]
+    end
+
+    def db
+      load_db
     end
 
     def load
